b1a7ae87831d2184de5d667ca9d61f30a965f6b6
Ivan <Ivanchen498@gmail.com>
2024-02-02 16:49:55 -0500

    functional remove button and flashcards created by user

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const createFC = document.querySelector('#createFC');
+const removeAll = document.querySelector('#removeAll');
 const cardContainer = document.querySelector('.card-container');
 const closeButton = document.querySelector('#closeButton');
 const saveButton = document.querySelector('#saveButton');
@@ -15,18 +16,85 @@ createFC.addEventListener('click', () => {
     cardContainer.style.display = 'block';
 });
 
+removeAll.addEventListener('click', ()=> {
+    flashContainer.textContent = '';
+    flashcardObjects.splice(0, flashcardIndex);
+    flashcardIndex = 0;
+})
+
 closeButton.addEventListener('click', () => {
     cardContainer.style.display = 'none';
 });
 
 questionText.addEventListener('input', ()=>{
     userQuestion = questionText.value;
+    
 })
 
 answerText.addEventListener('input', ()=>{
     userAnswer = answerText.value;
+
 })
 
 saveButton.addEventListener('click', () => {
+    if (flashcardIndex === 11) {
+        createFC.disabled = true;
+    }
+
+    const individualCard = document.createElement('div');
+    const questionContainer = document.createElement('div');
+    const answerContainer = document.createElement('div');
+    const flipbuttonContainer = document.createElement('div');
+    const optionsContainer = document.createElement('div');
+    const editButton = document.createElement('button');
+    const removeButton = document.createElement('button');
+    const showButton = document.createElement('button');
+    let showAnswer = false;
+
+    flashcardObjects.push({ question: userQuestion, answer: userAnswer});
+    questionText.value = '';
+    answerText.value = '';
+    userQuestion = '';
+    userAnswer = '';
+    cardContainer.style.display = 'none';
+
+    individualCard.classList.add('front');
+    questionContainer.classList.add('displayQuestion');
+    answerContainer.classList.add('showingAnswer');
+    showButton.classList.add('showButton');
+    optionsContainer.classList.add('editORdelete');
+
+    questionContainer.textContent = flashcardObjects[flashcardIndex++].question;
+    flipbuttonContainer.style.cssText = 'margin:0 auto; position:absolute; top:50%; left:50%; transform:translate(-50%, -50%)';
+    showButton.textContent = 'Show/Hide';
+    editButton.textContent = 'Edit';
+    removeButton.textContent = 'Remove';
+
+    optionsContainer.append(editButton);
+    optionsContainer.append(removeButton);
+    flipbuttonContainer.append(showButton);
+    individualCard.append(questionContainer);
+    individualCard.append(flipbuttonContainer);
+    individualCard.append(optionsContainer);
+    flashContainer.append(individualCard);
+
+    showButton.addEventListener('click', () => {
+        showAnswer = !showAnswer;
+
+        if (showAnswer === true) {
+            const correspondingQuestion = questionContainer.textContent;
+            flashcardObjects.forEach((item) => {
+                if (item.question == correspondingQuestion) {
+                    answerContainer.textContent = item.answer;
+                }
+            });
+
+            individualCard.append(answerContainer);        
+        }
+         else {
+            answerContainer.textContent = '';
+        }
+    });
 
 });
+
